8665b2501b379cd372015fbc524ffd999801dded
Dirgaaa <dirgayuditama6@gmail.com>
2026-09-06 10:31:52 +0700

    feat(profile): switch profile pic and add social platforms
    - Switch avatar to SVG
    
    - Add platform name to social links

--- shared/data/profile.ts
+++ shared/data/profile.ts
@@ -7,20 +7,27 @@ export const profile: Profile = {
 	bio: "I love creating elegant solutions to complex problems and constantly learning new technologies. I use Arch btw.",
 	description:
 		"I enjoy building modern frontend interfaces, caring about details, smooth interactions, and user experience. Always curious about new tools and better ways to build things.",
-	avatar: "/Avatar.jpeg",
+	avatar: "/Avatar.svg",
 	logo: "/LogoDirgaaBlack.svg",
 	location: "Malang, Indonesia",
 	email: "dirgayuditama6@gmail.com",
 	resumeUrl: "/resume.pdf",
 	availableForWork: true,
 	socials: [
-		{ name: "dirgaydtm", url: "https://github.com/dirgaydtm", icon: FaGithub },
 		{
+			platform: "GitHub",
+			name: "dirgaydtm",
+			url: "https://github.com/dirgaydtm",
+			icon: FaGithub,
+		},
+		{
+			platform: "LinkedIn",
 			name: "dirgaydtm",
 			url: "https://linkedin.com/in/dirgaydtm",
 			icon: FaLinkedin,
 		},
 		{
+			platform: "Instagram",
 			name: "dirgaa.yd",
 			url: "https://instagram.com/dirgaa.yd",
 			icon: FaInstagram,
